9b2857f67e5fd9a7e3ae8991a19b8bfabca5d113
Prasan Thukral <thukralprasan@gmail.com>
2024-04-18 11:41:19 +0530

    added \n to request logger

--- Utilities/requestLogger.js
+++ Utilities/requestLogger.js
@@ -5,7 +5,7 @@ const appendFile = promisify(fs.appendFile);
 
 const requestLogger = async (req, res, next) => {
   try {
-    const loggers = `${new Date()} - ${req.method} - ${req.url} `;
+    const loggers = `${new Date()} - ${req.method} - ${req.url} \n`;
     await appendFile("RequestLogger.log", loggers);
     next();
   } catch (error) {
